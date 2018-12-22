477c5622e0e013f21c2899029dcc3109b598ab0b
zannb <zannb@outlook.com>
2018-12-23 01:38:06 +0800

    base try to get json quote

--- first.go
+++ first.go
@@ -1,1 +1,30 @@
-package msg_flow_backend
+package main
+
+import (
+	"fmt"
+	"github.com/bitly/go-simplejson"
+	"io/ioutil"
+	"net/http"
+)
+
+const BaseUrl = "https://1token.trade/api/v1/quote"
+
+func Get_quote(endpoint string) {
+	fmt.Println(BaseUrl + endpoint)
+	resp, err := http.Get(BaseUrl + endpoint)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	js, err := simplejson.NewJson(body)
+	fmt.Println(js)
+
+}
+
+func main() {
+	Get_quote("/single-tick/okex/btc.usdt")
+}
